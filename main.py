32b9d7d634b3e25bbf3ecec6812c1d24fa1908c8
LeTrungKhai <letrungkhai20211@gmail.com>
2025-06-14 14:44:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.title("jasjdajsd")
+
+st.title("🎨 Đổi màu nền trang")
+r = st.slider("Red", 0, 255, 128)
+g = st.slider("Green", 0, 255, 128)
+b = st.slider("Blue", 0, 255, 128)
+
+st.markdown(
+    f"<div style='background-color: rgb({r},{g},{b}); height: 100px;'></div>",
+    unsafe_allow_html=True
+)
