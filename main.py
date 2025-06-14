2119ecb7156fd55bf85523ee26780755b1d33681
LeTrungKhai <letrungkhai20211@gmail.com>
2025-06-14 14:43:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 import streamlit as st
-import requests
-
-# URL tới file raw trên GitHub
-github_url = "https://raw.githubusercontent.com/username/repo/main/filename.py"
-
-# Gửi request và hiển thị nội dung
-response = requests.get(github_url)
-if response.status_code == 200:
-    code = response.text
-    st.code(code, language='python')
-else:
-    st.error("Không thể tải nội dung từ GitHub.")
+st.titletitle("jasjdajsd")
