1780ebf0d648e266317a3874e433586a6bce67c9
LeTrungKhai <letrungkhai20211@gmail.com>
2025-06-14 14:48:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-st.title("🎨 Đổi màu nền trang")
-r = st.slider("Red", 0, 255, 128)
-g = st.slider("Green", 0, 255, 128)
-b = st.slider("Blue", 0, 255, 128)
-
-st.markdown(
-    f"<div style='background-color: rgb({r},{g},{b}); height: 100px;'></div>",
-    unsafe_allow_html=True
-)
+st.title("👋 Xin chào!")
+name = st.text_input("Nhập tên của bạn:")
+if name:
+    st.success(f"Chào {name}, chúc bạn một ngày tốt lành!")
